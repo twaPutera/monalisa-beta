370217ee6188ac15f070b3bda5bd2fea454357c5
I Gede Aldi Wahyudi <aldiwahyudi@student.unud.ac.id>
2022-10-05 14:15:13 +0800

    template detail pemindahan asset

--- resources/views/layouts/user/master-detail.blade.php
+++ resources/views/layouts/user/master-detail.blade.php
@@ -16,20 +16,13 @@
     <!-- App Header -->
     <div class="appHeader bg-primary text-light">
         <div class="left">
-            <a href="#" class="headerButton" data-bs-toggle="modal" data-bs-target="#sidebarPanel">
-                <ion-icon name="menu-outline"></ion-icon>
-            </a>
+            @yield('back-button')
         </div>
-        <div class="pageTitle">@yield('page-title')</div>
+        <div class="pageTitle">Page Title</div>
         <div class="right">
-            <a href="app-notifications.html" class="headerButton">
-                <ion-icon class="icon" name="notifications-outline"></ion-icon>
-                <span class="badge badge-danger">4</span>
-            </a>
-            <a href="app-settings.html" class="headerButton">
-                <img src="/assets/user/img/sample/avatar/avatar1.jpg" alt="image" class="imaged w32">
-                <span class="badge badge-danger">6</span>
-            </a>
+            {{-- <a href="#" class="headerButton">
+                <ion-icon name="notifications-outline" role="img" class="md hydrated" aria-label="notifications outline"></ion-icon>
+            </a> --}}
         </div>
     </div>
     <!-- * App Header -->
@@ -53,12 +46,9 @@
 
     <!-- App Bottom Menu -->
     <div class="appBottomMenu">
-        @include('layouts.user.bottom-menu')
+        @yield('button-menu')
     </div>
     <!-- * App Bottom Menu -->
-
-    <!-- App Sidebar -->
-    @include('layouts.user.sidebar')
     <!-- * App Sidebar -->
 
     <!-- ========= JS Files =========  -->
