7cf4060f88231602e4bcb658790d7d9eba6ed1f7
I Gede Aldi Wahyudi <aldiwahyudi@student.unud.ac.id>
2022-10-19 11:25:17 +0800

    fix migration user

--- database/migrations/2022_10_18_075755_alter_table_user_add_colum.php
+++ database/migrations/2022_10_18_075755_alter_table_user_add_colum.php
@@ -14,7 +14,7 @@ class AlterTableUserAddColum extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->uuid('id')->primary()->change();
+            // $table->uuid('id')->primary()->change();
             // $table->uuid('guid')->after('email')->nullable();
             // $table->string('username_sso')->nullable()->after('guid');
             // $table->string('role', 50)->default('user')->after('email');
