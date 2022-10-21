ed66e3bec3d8ba4aa1ce37554d7631de34597bf6
deyan-ardi <riyan.clsg11@gmail.com>
2022-10-21 10:32:37 +0800

    fix pengaduan dashboard and bug

--- resources/views/pages/user/pengaduan/detail.blade.php
+++ resources/views/pages/user/pengaduan/detail.blade.php
@@ -161,30 +161,36 @@
                     </div>
                 </div>
             </div>
-            <div class="py-2 border-bottom border-secondary">
-                <div class="row">
-                    <div class="col">
-                        <p class="mb-0 text-green">Gambar Pengaduan Saya</p>
-                    </div>
-                    <div class="col  text-end">
-                        <a href="{{ route('user.pengaduan.download-gambar') . '?filename=' . $pengaduan->image[0]->path . '&status=request' }}"
-                            download class="btn btn-primary shadow-customD btn-sm mb-0"><i class="fa fa-download"></i> Unduh
-                            Gambar</a>
-                    </div>
-                </div>
-            </div>
-            <div class="py-2 border-bottom border-secondary">
-                <div class="row">
-                    <div class="col">
-                        <p class="mb-0 text-green">Gambar Respon Pengaduan</p>
-                    </div>
-                    <div class="col  text-end">
-                        <a href="{{ route('user.pengaduan.download-gambar') . '?filename=' . $pengaduan->image[1]->path . '&status=response' }}"
-                            download class="btn btn-primary shadow-customD btn-sm mb-0"><i class="fa fa-download"></i> Unduh
-                            Gambar</a>
+            @if (isset($pengaduan->image[0]))
+                <div class="py-2 border-bottom border-secondary">
+                    <div class="row">
+                        <div class="col">
+                            <p class="mb-0 text-green">Gambar Pengaduan Saya</p>
+                        </div>
+                        <div class="col  text-end">
+                            <a href="{{ route('user.pengaduan.download-gambar') . '?filename=' . $pengaduan->image[0]->path . '&status=request' }}"
+                                download class="btn btn-primary shadow-customD btn-sm mb-0"><i class="fa fa-download"></i>
+                                Unduh
+                                Gambar</a>
+                        </div>
                     </div>
                 </div>
-            </div>
+            @endif
+            @if (isset($pengaduan->image[1]))
+                <div class="py-2 border-bottom border-secondary">
+                    <div class="row">
+                        <div class="col">
+                            <p class="mb-0 text-green">Gambar Respon Pengaduan</p>
+                        </div>
+                        <div class="col  text-end">
+                            <a href="{{ route('user.pengaduan.download-gambar') . '?filename=' . $pengaduan->image[1]->path . '&status=response' }}"
+                                download class="btn btn-primary shadow-customD btn-sm mb-0"><i class="fa fa-download"></i>
+                                Unduh
+                                Gambar</a>
+                        </div>
+                    </div>
+                </div>
+            @endif
         </div>
     </div>
 @endsection
