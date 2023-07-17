7c95914dfac259863f5d3404fe373927a19866bd
deyan-ardi <riyan.clsg11@gmail.com>
2023-07-17 14:30:54 +0800

    change alert

--- resources/views/pages/user/scan/index.blade.php
+++ resources/views/pages/user/scan/index.blade.php
@@ -70,7 +70,7 @@
                     if (device.kind === 'videoinput' && !device.label.toLowerCase().includes('front')) {
                         backCameraId = device.deviceId;
                     }
-                    alert(device);
+                    alert(device.kind, device.label);
                 });
 
                 var scannerConfig = {
